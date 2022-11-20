f1fc36efe70095c40ff3bc8f770ba827250cf657
David Machado <machadodavid32@gmail.com>
2022-11-20 11:21:23 -0300

    Adicionando novas aulas de sentença, bloco de codigo e dados 01

---
+++ desafios/desafio_sentenca_bloco.js
@@ -0,0 +1,19 @@
+// Imprimar numeros impares e pares até o 10, sendo cada grupo dividido por dois blocos de código
+
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
+
+{
+
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+
+}
---
+++ inicio/bloco_codigo.js
@@ -0,0 +1,8 @@
+// Bloco de código
+
+{
+    console.log("passo 1");  // aqui se chama sentença de código
+    console.log("passo 2");
+    console.log("passo  3");
+}  // aqui se chama bloco de codigo, é uma junção de sentenas dentro de uma chave.
+
